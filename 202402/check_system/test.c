5471604c8d7af920a9275a22fc8ceab57e365972
isprime <maomaoyeah520@163.com>
2024-02-17 22:20:49 +0800

    使用联合体判断大小端。

--- 202402/check_system/test.c
+++ 202402/check_system/test.c
@@ -3,6 +3,8 @@
 //
 #include <stdio.h>
 
+/*
+//方法1
 int check_sys()
 {
     int a = 1;
@@ -18,3 +20,28 @@ int main(void)
 
     return 0;
 }
+*/
+
+//方法2
+int check_sys()
+{
+    union {
+        char c;
+        int i;
+    } u;
+    u.i = 1;
+    return u.c;
+}
+
+
+int main(void)
+{
+    if (check_sys() == 1) {
+        printf("小端\n");
+    } else {
+        printf("大端\n");
+    }
+
+    return 0;
+}
+
